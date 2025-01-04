b06ad14b2df1d2c118e8f217d5679271e160d8fe
Tas-em <gursamet125@outlook.com>
2025-01-04 10:48:27 +0300

    Inıtıal commit

---
+++ src/main/java/net/tasem/tutmod/TasemModClient.java
@@ -0,0 +1,10 @@
+package net.tasem.tutmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TasemModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
---
+++ src/main/java/net/tasem/tutmod/TasemModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.tasem.tutmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TasemModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
